fb555dfd49427a53ad5d730191ecde6997566674
Max Lystopadov <senior.listik@gmail.com>
2023-03-11 20:48:34 +0200

    reorganization of files and folders

---
+++ js/main.js
@@ -0,0 +1,28 @@
+const rowKey = document.querySelector('.row__key');
+        const rowCode = document.querySelector('.row__code');
+        const rowWhich = document.querySelector('.row__which');
+        const keyHistory = document.querySelector('.row__history');
+        const arr = [];
+
+        document.body.addEventListener('keydown', function(event){
+            rowKey.innerText = event.key;
+            rowCode.innerText = event.code;
+            rowWhich.innerText = event.which;
+
+            arr.unshift(event.key);
+
+            let keyBlock = document.createElement('div');
+            keyBlock.classList.add('key')
+            let firstElement = keyHistory.firstChild;
+        
+            arr.reverse().forEach((el, i) => {
+
+                keyBlock.innerHTML = el;
+                keyHistory.insertBefore(keyBlock, firstElement);
+
+                if (i > 3) {
+                    arr.pop();
+                    keyHistory.lastElementChild.remove();
+                }
+            });
+        })
