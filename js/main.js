0007accf9bb05137f048bd0d9583e9fbf3c6db24
Max Lystopadov <senior.listik@gmail.com>
2023-03-11 21:46:41 +0200

    typed html structure

--- js/main.js
+++ js/main.js
@@ -1,28 +1,40 @@
 const rowKey = document.querySelector('.row__key');
-        const rowCode = document.querySelector('.row__code');
-        const rowWhich = document.querySelector('.row__which');
-        const keyHistory = document.querySelector('.row__history');
-        const arr = [];
+const rowCode = document.querySelector('.row__code');
+const rowWhich = document.querySelector('.row__which');
+const keyHistory = document.querySelector('.row__history');
+const arr = [];
 
-        document.body.addEventListener('keydown', function(event){
-            rowKey.innerText = event.key;
-            rowCode.innerText = event.code;
-            rowWhich.innerText = event.which;
+document.body.addEventListener('keydown', function(event){
+    rowKey.innerText = event.key;
+    rowCode.innerText = event.code;
+    rowWhich.innerText = event.which;
+    console.log(event);
+    arr.unshift(event.key);
 
-            arr.unshift(event.key);
+    let keyBlock = document.createElement('div');
+    keyBlock.classList.add('key')
+    let firstElement = keyHistory.firstChild;
 
-            let keyBlock = document.createElement('div');
-            keyBlock.classList.add('key')
-            let firstElement = keyHistory.firstChild;
-        
-            arr.reverse().forEach((el, i) => {
+    arr.reverse().forEach((el, i) => {
 
-                keyBlock.innerHTML = el;
-                keyHistory.insertBefore(keyBlock, firstElement);
+        keyBlock.innerHTML = el;
+        keyHistory.insertBefore(keyBlock, firstElement);
 
-                if (i > 3) {
-                    arr.pop();
-                    keyHistory.lastElementChild.remove();
-                }
-            });
-        })
+        if (i > 3) {
+            arr.pop();
+            keyHistory.lastElementChild.remove();
+        }
+    });
+})
+// key: "t"
+// event.location
+// event.code
+// event.which
+// history
+
+//  <div class="row">
+//         <div class="row__key"></div>
+//         <div class="row__code"></div>
+//         <div class="row__which"></div>
+//         <div class="row__history"></div>
+//     </div>
